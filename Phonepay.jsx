a14f15f9d2fa14546697579921bdf5f7ea532606
Shruthip <120468869+Shruthi1613@users.noreply.github.com>
2023-03-02 22:43:05 +0530

    Add files via upload

---
+++ Phonepay.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Component } from "react";
+
+class Phonepay extends Component
+{
+    render()
+    {
+        return <h1>Transcation amt is 20000</h1>
+}
+}
+export default Phonepay
