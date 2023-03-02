b8ec050014618ac35dd2baf85d2a1645ee03b8ce
Shruthip <120468869+Shruthi1613@users.noreply.github.com>
2023-03-02 22:43:34 +0530

    Add files via upload

---
+++ Zomato.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Component } from "react";
+
+class Zomato extends Component
+{
+    render()
+    {
+        return <h1>Paneer Butter Masala</h1>
+}
+}
+export default Zomato
