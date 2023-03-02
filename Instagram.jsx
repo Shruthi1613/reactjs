5255a6dca8648f2b9ee13c698318a62fb3e14819
Shruthip <120468869+Shruthi1613@users.noreply.github.com>
2023-03-02 22:41:57 +0530

    Add files via upload

---
+++ Instagram.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Component } from "react";
+
+class Instagram extends Component
+{
+    render()
+    {
+        return <h1>Reels</h1>
+        }
+}
+export default Instagram
