96cb7b857b58cf7f79ffe13d4ce1ec8d0b1ffff2
Shruthip <120468869+Shruthi1613@users.noreply.github.com>
2023-03-02 22:41:29 +0530

    Add files via upload

---
+++ Chats.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Component } from "react";
+
+class Chats extends Component
+{
+    render()
+    {
+        return <h1>Hiiii</h1>
+    }
+}
+export default Chats
