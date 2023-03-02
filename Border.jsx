e31535a065cffb1feaa04781f5701348f7b2d8a9
Shruthip <120468869+Shruthi1613@users.noreply.github.com>
2023-03-02 22:41:04 +0530

    Add files via upload

---
+++ Border.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Component } from "react";
+
+class Border extends Component
+{
+    render()
+    {
+        return <h1>{2*10}</h1>
+}
+}
+export default Border
