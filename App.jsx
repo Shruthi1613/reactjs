2a0d7e365bb14175b24057c663d4abd25622c9ed
Shruthip <120468869+Shruthi1613@users.noreply.github.com>
2023-03-05 23:51:54 +0530

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,8 +1,25 @@
-import React from "react";
+import React from 'react'
+import Nav from './Nav'
+// import './global.css'
+import './Navbar1css.css'
+// import STYLE from "./axis.module.css";
+// import Navbar1 from './Navbar1';
+//  import Zomato1 from './Zomato1';
+// import Hotstar1 from './Hotstar1'
+// import Amazon from './Amazon';
+import Axis from './Axis';
 
 const App = () => {
-    return (
-        <div>App</div>
-    )
+  return (
+    <>
+   {/* <Nav/> */}
+    {/* <Navbar1/> */}
+    {/* <Zomato1/> */}
+    {/* <Hotstar1/> */}
+    {/* <Amazon/> */}
+    <Axis/>
+   </>
+  )
 }
+
 export default App
