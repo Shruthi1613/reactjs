b8a83c28164b3424a61ee491bf517b8d0e40cb09
Shruthip <120468869+Shruthi1613@users.noreply.github.com>
2023-03-05 23:53:07 +0530

    Add files via upload

---
+++ Amazon.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+
+
+const Amazon = () => {
+  return (
+    <>
+ <div className='amaze'>
+    <nav className='naviBar'>
+        <div>
+            <img src="https://img.favpng.com/24/16/11/amazon-web-services-logo-cloud-computing-amazon-com-infrastructure-as-code-png-favpng-2Zr03R7hCiJAQCC7eSFiGm4Yx.jpg" alt="" />
+        </div>
+ <div className='Menbar'>
+    <ul>
+        <li>
+            <a href="">Home</a>
+        </li>
+        <li>
+            <a href="">Signin</a>
+        </li>
+        <li>
+            <a href="">Add To Cart</a>
+        </li>
+        <li>
+            <a href="">My Orders</a>
+        </li>
+        <li>
+            <a href="">Wishlist</a>
+        </li>
+    </ul>
+ </div>
+ </nav>
+   </div> 
+
+   </>
+  )
+}
+
+export default Amazon
