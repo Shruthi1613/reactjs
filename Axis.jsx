93e090bea3980b90f8000f08c30c126a8248bfbb
Shruthip <120468869+Shruthi1613@users.noreply.github.com>
2023-03-05 23:51:34 +0530

    Add files via upload

---
+++ Axis.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+
+const Axis = () => {
+  return (
+    <>
+ <div className='ax'>
+    <nav className='naviBar'>
+        <div className='axises'>
+            <img src="https://tse2.mm.bing.net/th?id=OIP.sZRJ8mBkwzsrflQHTgoRSAHaCA&pid=Api&P=0" alt="" />
+        </div>
+ <div className='Menbar'>
+    <ul>
+        <li>
+            <a href="">Home</a>
+        </li>
+        <li>
+            <a href="">Signin</a>
+        </li>
+        <li>
+            <a href="">Amount Transcation</a>
+        </li>
+        <li>
+            <a href="">Account Balance</a>
+        </li>
+        <li>
+            <a href="">Account Info</a>
+        </li>
+    </ul>
+ </div>
+ </nav>
+   </div> 
+ </>
+  )
+}
+
+export default Axis
