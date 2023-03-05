ff650bc8def68e74f26c523349ae6455fd843ee8
Shruthip <120468869+Shruthi1613@users.noreply.github.com>
2023-03-05 23:51:06 +0530

    Add files via upload

---
+++ Zomato1.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+
+
+const Zomato1 = () => {
+  return (
+    <>
+ <div className='zoma'>
+    <nav className='naviBar'>
+        <div>
+            <img src="https://tse1.mm.bing.net/th?id=OIP.JawH8d_YtSw_oH6Eel0miAHaEK&pid=Api&P=0" alt="" />
+        </div>
+ <div className='Menbar'>
+    <ul>
+        <li>
+            <a href="">Home</a>
+        </li>
+        <li>
+            <a href="">Signin</a>
+        </li>
+        <li>
+            <a href="">Menu</a>
+        </li>
+        <li>
+            <a href="">My Orders</a>
+        </li>
+        <li>
+            <a href=""> Profile</a>
+        </li>
+    </ul>
+ </div>
+ </nav>
+   </div> 
+
+   </>
+  )
+}
+
+export default Zomato1
