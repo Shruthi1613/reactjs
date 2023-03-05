180e6e50f79202c40ae266870373e466bd838ca9
Shruthip <120468869+Shruthi1613@users.noreply.github.com>
2023-03-05 23:52:46 +0530

    Add files via upload

---
+++ Hotstar1.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+
+
+const Hotstar1 = () => {
+  return (
+    <>
+ <div className='hots'>
+    <nav className='naviBar'>
+        <div className='star'>
+            <img src="https://tse1.explicit.bing.net/th?id=OIP.5gnrYPWevoYsyFzUraDHWgHaEo&pid=Api&P=0" alt="" />
+        </div>
+ <div className='Menbar'>
+    <ul>
+        <li>
+            <a href="">Home</a>
+        </li>
+        <li>
+            <a href="">Signin</a>
+        </li>
+        <li>
+            <a href="">Movies</a>
+        </li>
+        <li>
+            <a href="">WebSeries</a>
+        </li>
+        <li>
+            <a href=""> Profile</a>
+        </li>
+    </ul>
+ </div>
+ </nav>
+   </div> 
+
+   </>
+  )
+}
+
+export default Hotstar1
