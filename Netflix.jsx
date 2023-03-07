c87a25867b652bb943671228fd65d698457dc42c
Shruthip <120468869+Shruthi1613@users.noreply.github.com>
2023-03-07 10:27:07 +0530

    Add files via upload

---
+++ Netflix.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+
+const Netflix = () => {
+  return (
+    <>
+ <div className='ax'>
+    <nav className='naviBar'>
+        <div className='axises'>
+            <img src="https://tse4.mm.bing.net/th?id=OIP.iUuKofalq-s9wS_kfq9e8QHaFj&pid=Api&P=0" alt="" />
+        </div>
+ <div className='Menbar'>
+    <ul>
+        <li>
+            <a href="">Home</a>
+        </li>
+        <li>
+            <a href="">Signin</a>
+        </li>
+        <li>
+            <a href="">Web Series</a>
+        </li>
+        <li>
+            <a href="">Movies</a>
+        </li>
+        <li>
+            <a href="">Logout</a>
+        </li>
+    </ul>
+ </div>
+ </nav>
+   </div> 
+ </>
+  )
+}
+
+export default Netflix
