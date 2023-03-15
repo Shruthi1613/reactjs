422cd100057ae6767cfe3146e72e5b6fa03c99e8
Shruthip <120468869+Shruthi1613@users.noreply.github.com>
2023-03-16 00:30:56 +0530

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,15 +1,47 @@
-import React from 'react'
-import Course from './Course'
+import React,{Component} from "react"
+import "./global2.css"
 
 
-const App = () => {
-  return (
-    <Course Coursedetails={{
-      CourseName:"Java Full Stack",
-      CourseAvail:true,
-      CourseDur:"6 Months"
-    }}/>
-  )
-}
+class App extends Component
+{
+  constructor()
+  {
+    super()
+    this.state={
+      count : 0}
+  }
+
+  handleIncrement=()=> {
+    this.setState({count : this.state.count+1})
+  }
+
+  handledecrement=()=>{
+    this.setState({count : this.state.count-1})
+  }
+
+  handleReset=()=> {
+    this.setState({count:0})
+  }
+
+  render()
+  {
+    return (
+      <>
+      <global/>
+      <div className="mainBlock">
+        <h1>{this.state.count}</h1>
+        <div className="Block">
+          <button onClick={this.handleIncrement}>+ Increment</button>
+          <button onClick={this.handledecrement}>- Decrement</button>
+          <button onClick={this.handleReset}>Reset</button>  
+          
+      </div>
+      </div>
+      </>
+    )
+
+    }
+  }
+
 
 export default App
